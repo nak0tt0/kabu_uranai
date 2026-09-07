ec713dc1222b58b625aeb361e7ecefa4a653d740
nak0tt0 <nak0tt0.biz.it@gmail.com>
2026-09-08 04:21:45 +0900

    mk_sessions

--- test/controllers/admin/dashboards_controller_test.rb
+++ test/controllers/admin/dashboards_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class Admin::DashboardsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get admin_dashboards_show_url
-    assert_response :success
+    get admin_dashboard_url
+    # 未ログイン時はログイン画面へリダイレクトされるか、またはレスポンス確認
+    assert_response :redirect
   end
 end
--- test/controllers/admin/sessions_controller_test.rb
+++ test/controllers/admin/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Admin::SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get admin_sessions_new_url
+    get admin_login_url
     assert_response :success
   end
 end
